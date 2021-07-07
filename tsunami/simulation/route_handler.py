92a3894ed6a9ec4b57fcbe10b1a40eca39eee409
Giuseppe <giuseppe.magazu@gmail.com>
2021-07-07 17:02:46 +0200

    upload latex template

---
+++ tsunami/simulation/route_handler.py
@@ -0,0 +1,11 @@
+# Find the route for a specific agent based on the queues
+
+class RouteHandler:
+    def __init__(self, graph, agent):
+        self.G = graph
+        self.agent = agent
+
+        pass
+
+    def get_next_intersection(self):
+        pass
