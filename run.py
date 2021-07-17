0e200b48b5ad4f796daad99eff63c4cba9a1cd5f
Giuseppe <giuseppe.magazu@gmail.com>
2021-07-17 16:19:33 +0200

    init test population and plot street

--- run.py
+++ run.py
@@ -1,21 +1,31 @@
 import os
 
-import osmnx as ox
 import geopandas as gpd
+import osmnx as ox
 
 from tsunami.config import DATA_DIR
 from tsunami.simulation.model import EvacuationModel
+from tsunami.simulation.plot import ModelViewer
 
 G = ox.load_graphml(os.path.join(DATA_DIR, "graph.xml"))
 
 population = gpd.read_file(os.path.join(DATA_DIR, "population.gpkg"))
 
-#N = 100
-#for k in range(N):
+# TEST
+population = population.head(1)
+population["population"][0] = 1
 
-model = EvacuationModel(G, 5, 100)
+model = EvacuationModel(G, 5, 5)
 model.init_state(population=population, shelters=None, tsunami=None)
-# model.run_iteration()
 
-# plot things!
-model.plot_graph()
+model.agents[0].orig = list(G)[10]
+model.agents[0].dest = list(G)[0]
+model.compute_route(model.agents[0])
+print(model.agents[0].route)
+
+model.run_iteration()
+
+viewer = ModelViewer(model)
+viewer.plot_street()
+# viewer.plot_population()
+# fig, ax = viewer.plot_route(model.agents[0].route, ax)
