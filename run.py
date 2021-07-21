5d9eacb8de4178100844d7db22154d9531cac973
Giuseppe <giuseppe.magazu@gmail.com>
2021-07-21 15:01:30 +0200

    add interactive plot

--- run.py
+++ run.py
@@ -7,25 +7,58 @@ from tsunami.config import DATA_DIR
 from tsunami.simulation.model import EvacuationModel
 from tsunami.simulation.plot import ModelViewer
 
+
+def compute_route_stats(agent):
+    path = agent.route
+
+    total_travel_time = 0
+    total_length = 0
+    avg_velocity = 0
+    for u, v in zip(path[0:], path[1:]):
+        edge_data = G.get_edge_data(u, v)
+        costs = [edge_data[edge]['cost'] for edge in edge_data]
+
+        min_cost = min(costs)
+        min_idx = costs.index(min_cost)
+        min_length = edge_data[min_idx]['length']
+
+        velocity = edge_data[min_idx]['link'].v
+
+        avg_velocity += velocity
+        total_travel_time += min_cost
+        total_length += min_length
+    total_travel_time /= 3600
+    total_length /= 1000
+    avg_velocity /= len(path) - 1
+    print(
+        f"travel_time: {total_travel_time:.2f}h\t length: {total_length:.2f}Km\t average velocity: {avg_velocity:.2f}m/s")
+
+
 G = ox.load_graphml(os.path.join(DATA_DIR, "graph.xml"))
 
-population = gpd.read_file(os.path.join(DATA_DIR, "population.gpkg"))
+population = gpd.read_file(os.path.join(DATA_DIR, "districts.gpkg"))
 
 # TEST
-population = population.head(1)
-population["population"][0] = 1
+# population = population.head(1)
+# population["population"][0] = 1
 
-model = EvacuationModel(G, 5, 5)
+model = EvacuationModel(G, 5, 10)
 model.init_state(population=population, shelters=None, tsunami=None)
 
-model.agents[0].orig = list(G)[10]
-model.agents[0].dest = list(G)[0]
-model.compute_route(model.agents[0])
-print(model.agents[0].route)
+a0 = model.agents[0]
+a0.orig = list(G)[13]
+a0.dest = list(G)[1]
 
-model.run_iteration()
+model.compute_route(a0)
+compute_route_stats(a0)
+
+# model.run_iteration()
+
+model.compute_route(a0)
+compute_route_stats(a0)
 
 viewer = ModelViewer(model)
-viewer.plot_street()
-# viewer.plot_population()
-# fig, ax = viewer.plot_route(model.agents[0].route, ax)
+# viewer.interactive_graph()
+# viewer.plot_street()
+# viewer.plot_population(save=True)
+viewer.plot_route(a0.route, save=True, show=False)
--- tsunami/simulation/plot.py
+++ tsunami/simulation/plot.py
@@ -55,7 +55,8 @@ class ModelViewer:
         self.__add_polygon(boundary, ax, cmap=cmap, lw=0, alpha=0.5, zorder=-1)
         self.__add_polygon(boundary, ax, fc="None", lw=2, alpha=1, zorder=1)
 
-        ctx.add_basemap(ax, zoom=13, source=ctx.providers.Esri.WorldImagery, reset_extent=False, zorder=-2)
+        ctx.add_basemap(ax, zoom=13, source=ctx.providers.Esri.WorldImagery, reset_extent=False,
+                        zorder=-2)
 
         ax.margins(0)
         ax.get_xaxis().set_visible(False)
@@ -80,7 +81,8 @@ class ModelViewer:
         fig, ax = ox.plot_graph_route(self.model.G, route,
                                       orig_dest_size=50,
                                       route_linewidth=2,
-                                      node_size=0)
+                                      node_size=0,
+                                      show=False)
 
         # load boundary polygon
         path = os.path.join(DATA_DIR, "safe_area.gpkg")
@@ -97,6 +99,83 @@ class ModelViewer:
             fig.show()
 
         return fig, ax
+
+    def interactive_graph(self):
+        fig, ax1 = ox.plot_graph(self.model.G, figsize=(12, 12), show=False, node_alpha=0.7,
+                                 node_size=10)
+
+        selected_points, = ax1.plot([], [], ms=5, color='r', marker='o', lw=0)
+        selected_lines, = ax1.plot([], [], ms=0, color='r')
+        info_text = ax1.annotate("", xy=(0.6, 0.05), xycoords='axes fraction', ha="left",
+                                 backgroundcolor="w",
+                                 bbox={'facecolor': 'white', 'alpha': 0.8, 'pad': 8})
+
+        def onclick(event):
+            if not event.dblclick:
+                if event.button == 1:
+                    point = (event.xdata, event.ydata)
+                    nn, d = ox.nearest_nodes(self.model.G, point[0], point[1], return_dist=True)
+
+                    if d > 150:
+                        print("no nodes found")
+                        return
+
+                    node = self.model.G.nodes[nn]
+
+                    info_text.set_text((
+                        f"id: {nn}\n"
+                        f"lat: {node['lat'] if 'lat' in node else '-'}\n"
+                        f"lon: {node['lon'] if 'lon' in node else '-'}\n"
+                        f"agents: {len(node['agents'])}\n"
+                    ))
+
+                    selected_points.set_data([node['x']], [node['y']])
+                    selected_lines.set_data([], [])
+
+                    fig.canvas.draw()
+                    fig.canvas.flush_events()
+                elif event.button == 3:
+                    point = (event.xdata, event.ydata)
+                    print("searching...")
+                    ne, d = ox.nearest_edges(self.model.G, point[0], point[1],
+                                             return_dist=True)
+
+                    if d > 150:
+                        print("no edges found")
+                        return
+
+                    edge = self.model.G.edges[ne]
+                    x, y = edge['geometry'].xy
+                    selected_points.set_data([x[0], x[len(x) - 1]], [y[0], y[len(x) - 1]])
+                    selected_lines.set_data(x, y)
+
+                    info_text.set_text((
+                        "Edge Info\n"
+                        f"id: {ne}\n"
+                        f"highway: {edge['highway'] if 'highway' in edge else '-'}\n"
+                        f"oneway: {edge['oneway'] if 'oneway' in edge else '-'}\n"
+                        f"length: {str(round(edge['length'], 2)) + ' m' if 'length' in edge else '-'}\n"
+                        f"min width: {edge['link'].w:.2f} m\n"
+                        f"area: {edge['link'].a:.2f} m^2\n"
+                        f"cost: {edge['cost']:.2f}\n\n"
+                        "Queue Info\n"
+                        f"velocity: {edge['link'].v:.2f} m/s\n"
+                        f"storage capacity: {edge['link'].c} ped\n"
+                        f"flow capacity: {edge['link'].q:.2f} ped/s\n"
+                        f"travel time: {edge['link'].t:.2f} s\n"
+                        f"queue used: {edge['link'].get_queue_used()}%"
+                    ))
+
+                    fig.canvas.draw()
+                    fig.canvas.flush_events()
+
+        cid = fig.canvas.mpl_connect("button_press_event", onclick)
+
+        fig.tight_layout()
+        plt.show()
+
+        input("Press any key to close...")
+        fig.canvas.mpl_disconnect(cid)
 
     @staticmethod
     def __add_polygon(gdf, ax, cmap=None, **kwargs):
